684a8f29e751867b35a50139b315c697130415ff
Darshit-dev <khenidarshit@gmail.com>
2022-04-06 12:32:41 +0530

    changes file

---
+++ W3/Day-17/selfpractice.js
@@ -0,0 +1,8 @@
+operations = ["--X","X++","X++"]
+
+var finalValueAfterOperations = function(operations) {
+    for(let i=0;i<operations.length;i++){
+        console.log(operations[i])
+    }
+};
+finalValueAfterOperations(operations)
